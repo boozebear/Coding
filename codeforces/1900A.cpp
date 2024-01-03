7df64b5136cfde07d4346c746e9c77328a8e8708
boozebear <mboozebear@outlook.com>
2024-01-03 15:23:38 +0530

    upload

---
+++ codeforces/1900A.cpp
@@ -0,0 +1,37 @@
+//date: January 3, 2024
+//cf-handle : boozebear
+//problem: 1900A - Cover in Water
+
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int n;
+    string s;
+
+    cin >> n >> s;
+
+    int ct = 0;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '.')ct++;
+        else{
+            if(ct>=2)ans+=2;
+            if(ct == 1)ans++;
+            ct = 0;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)solve();
+
+    return 0;
+}
